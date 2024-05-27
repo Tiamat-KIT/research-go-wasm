1846eb32ffdcd20d12a5a05174671e627f29f0ce
fill.ayaextech <fill.ayaextech@gmail.com>
2024-05-27 07:55:42 +0000

    行列計算を行う上での効率的なルーチンを用いたアルゴリズムを考え、実装を行うことにしたい

--- go-wasm/main.go
+++ go-wasm/main.go
@@ -9,7 +9,7 @@ func add(a int,b int) int{
 
 //export multiply
 func multiply(a int,b int) int{
-	return a*b
+	return a*b Z
 }
 
 //export divide
@@ -21,3 +21,18 @@ func divide(a int,b int) int{
 func subtract(a int,b int) int{
 	return a-b
 }
+
+//export square
+func square(a int) int{
+	return a*a
+}
+
+//export matrix_add
+func matrix_add(a [][]int,b [][]int) [][]int{
+	// 数値の配列の配列で構成されていると考えた時、
+	// 行列の加算を行う関数
+	// 対応する行ごとの要素の加算を行うことは
+	// 動的なルーチン作成によって、高速な行列計算を実現できる。
+
+}
+
