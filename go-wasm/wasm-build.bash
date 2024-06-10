08ddab23e96b186c36eda5106dc70d133d75203b
fill.ayaextech <fill.ayaextech@gmail.com>
2024-06-10 10:23:22 +0000

    いらないファイルの整理

--- go-wasm/wasm-build.bash
+++
@@ -1,5 +0,0 @@
-DATE=`date '+%Y-%m-%d'`
-FOLDER=dist-$DATE
-mkdir $FOLDER
-cp "$(tinygo env TINYGOROOT)/targets/wasm_exec.js" ./$FOLDER/wasm_exec.js
-tinygo build -o ./$FOLDER/output.wasm -target wasm ./main.go
